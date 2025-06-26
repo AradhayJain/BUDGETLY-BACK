82c439b2dfd12bd6c4ce679460970a74c6845fa3
AradhayJain <153743476+AradhayJain@users.noreply.github.com>
2025-06-26 13:16:34 +0530

    Update groups.routes.js

--- backend/routes/groups.routes.js
+++ backend/routes/groups.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {protect} from "../middlewares/authMiddleware.js"
-import { addExpense, createGroup, deleteGroup, fetchAll } from "../controllers/group.controller.js"
+import { addExpense, changeGroupBudget, createGroup, deleteGroup, fetchAll } from "../controllers/group.controller.js"
 import { getMessages, sendMessage } from "../controllers/message.controller.js"
 
 const router = express.Router()
@@ -11,5 +11,6 @@ router.post('/delete',protect,deleteGroup);
 router.post('/send-message',protect,sendMessage);
 router.get('/get-messages',protect,getMessages);
 router.post('/add-expense',protect,addExpense);
+router.post('/change-budget',protect,changeGroupBudget);
 
 export default router;
